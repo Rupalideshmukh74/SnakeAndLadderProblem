cbe0804b98a92aa7c40aae9776502add6a417218
Rupalideshmukh74 <deshmukhrupali39@gmail.com>
2021-08-06 15:48:42 +0530

    [Rupali]Refactor:Player reaches the winning position

--- Program.cs
+++ Program.cs
@@ -7,30 +7,63 @@ namespace snakeAndLadder
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Snake And Ladder Problem");
-            int startPoint = 0;
+           
             int position = 0;
             Random random = new Random();
             int rollDieNo = random.Next(1, 7);
             int optionOfGame = random.Next(0, 3);
             Console.WriteLine("Roll Die Number " + rollDieNo);
-            switch (optionOfGame)
+
+            while (position != 100)
             {
-                case 0:
-                    Console.WriteLine("Position : " + position);
-                    break;
-                case 1:
-                    position += rollDieNo;
-                    Console.WriteLine("Position : " + position);
-                    break;
+                // generating Random Numbers for Die and Game Options
+                rollDieNo = random.Next(1, 7);
+                optionOfGame = random.Next(0, 3);
 
-                case 2:
-                    position -= rollDieNo;
-                    Console.WriteLine("Position : " + position);
-                    break;
+                switch (optionOfGame)
+                {
+                    case 0:
+                        Console.WriteLine("Position : " + position);
+                        break;
+                    case 1:
+                        position += rollDieNo;
+                        Console.WriteLine("Position : " + position);
+                        break;
 
-                default:
-                    break;
-            }  
+                    case 2:
+                        position -= rollDieNo;
+                        Console.WriteLine("Position : " + position);
+                        break;
+
+                        switch (optionOfGame)
+                        {
+                            case 0:
+                                Console.WriteLine("Position : " + position);
+                                break;
+                            case 1:
+                                position += rollDieNo;
+                                Console.WriteLine("Position : " + position);
+                                break;
+
+                            case 2:
+                                position -= rollDieNo;
+                                Console.WriteLine("Position : " + position);
+                                break;
+
+                            default:
+                                break;
+                        }
+                        if (position > 100)
+                            break;
+                        else if (position < 0)
+                            position = 0;
+                }
+                //Display Position Number
+                Console.WriteLine("You Won ! Reached at : " + position);
+            }
         }
+           
     }
 }
+   
+
