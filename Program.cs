3c71ef657eb865ada665da6996a05f5a14b930d6
Rupalideshmukh74 <deshmukhrupali39@gmail.com>
2021-08-06 15:16:25 +0530

    [Rupali]Refactor:player roll the die

--- Program.cs
+++ Program.cs
@@ -8,6 +8,29 @@ namespace snakeAndLadder
         {
             Console.WriteLine("Welcome to Snake And Ladder Problem");
             int startPoint = 0;
+            int position = 0;
+            Random random = new Random();
+            int rollDieNo = random.Next(1, 7);
+           // int optionOfGame = random.Next(0, 3);
+            Console.WriteLine("Roll Die Number " + rollDieNo);
+           /* switch (optionOfGame)
+            {
+                case 0:
+                    Console.WriteLine("Position : " + position);
+                    break;
+                case 1:
+                    position += rollDieNo;
+                    Console.WriteLine("Position : " + position);
+                    break;
+
+                case 2:
+                    position -= rollDieNo;
+                    Console.WriteLine("Position : " + position);
+                    break;
+
+                default:
+                    break;
+            }  */  
         }
     }
 }
